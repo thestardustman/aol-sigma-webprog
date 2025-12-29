18bc38f890e0f2b6ac54fb73a50a1355de825522
thestardustman <satyadarma511@gmail.com>
2025-12-29 13:09:31 +0700

    fix: restore User model with getProfileCompletionPercentage method from ver_satya

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -40,25 +34,16 @@ class User extends Authenticatable
         'kyc_rejection_reason',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'kyc_verified_at' => 'datetime',
             'password' => 'hashed',
             'is_admin' => 'boolean',
         ];
@@ -109,5 +94,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Donation::class);
     }
-    
 }
