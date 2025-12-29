d252f35bce403edbc0ace39c0f142301af8ff7bc
thestardustman <satyadarma511@gmail.com>
2025-12-29 15:22:39 +0700

    feat: replace faker campaign data with realistic Indonesian charity campaigns

--- database/factories/CampaignFactory.php
+++ database/factories/CampaignFactory.php
@@ -23,12 +23,53 @@ class CampaignFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'title' => $this->faker->sentence(3), // Judul kegiatan (3 kata)
-            'community_name' => $this->faker->company(),
-            'description' => $this->faker->paragraph(3),
-            'img' => 'https://via.placeholder.com/640x480.png/007bff?text=SDG+Hope', // Gambar dummy
-            'date' => $this->faker->dateTimeBetween('now', '+1 year'),
+        static $campaigns = [
+            [
+                'title' => 'Bangun Sekolah untuk Anak Pelosok Kalimantan',
+                'community_name' => 'Yayasan Pendidikan Nusantara',
+                'description' => 'Mari bersama membangun fasilitas sekolah yang layak untuk 300 anak di pedalaman Kalimantan. Dengan dukungan Anda, mereka akan mendapatkan akses pendidikan yang lebih baik, ruang kelas yang nyaman, dan buku pelajaran yang memadai.',
+                'img' => 'https://images.unsplash.com/photo-1503676260728-1c00da094a0b?w=640&h=480&fit=crop',
+                'date' => '2025-03-15',
+                'target' => 50000000,
+            ],
+            [
+                'title' => 'Air Bersih untuk Desa Terpencil NTT',
+                'community_name' => 'Komunitas Peduli Air Bersih',
+                'description' => 'Ribuan warga di NTT masih kesulitan mendapatkan air bersih. Program ini akan membangun sumur bor dan sistem penyaringan air untuk 5 desa. Setiap tetes donasi Anda adalah harapan hidup yang lebih sehat bagi mereka.',
+                'img' => 'https://images.unsplash.com/photo-1541591838774-46ce1d4e4e14?w=640&h=480&fit=crop',
+                'date' => '2025-04-20',
+                'target' => 35000000,
+            ],
+            [
+                'title' => 'Bantuan Medis untuk Penderita Kanker Anak',
+                'community_name' => 'Rumah Harapan Indonesia',
+                'description' => 'Kami mengumpulkan dana untuk biaya pengobatan dan perawatan 50 anak penderita kanker dari keluarga kurang mampu. Setiap donasi memberikan kesempatan hidup dan harapan sembuh bagi buah hati Indonesia.',
+                'img' => 'https://images.unsplash.com/photo-1578496479763-c21c718af028?w=640&h=480&fit=crop',
+                'date' => '2025-05-10',
+                'target' => 75000000,
+            ],
+            [
+                'title' => 'Rehabilitasi Hutan Mangrove Pesisir Jawa',
+                'community_name' => 'Gerakan Hijau Nusantara',
+                'description' => 'Program penanaman 10,000 pohon mangrove untuk melindungi pesisir dari abrasi dan meningkatkan ekosistem laut. Mari jaga lingkungan untuk generasi mendatang dengan aksi nyata dimulai hari ini.',
+                'img' => 'https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?w=640&h=480&fit=crop',
+                'date' => '2025-06-05',
+                'target' => 25000000,
+            ],
+            [
+                'title' => 'Beasiswa Yatim Berprestasi',
+                'community_name' => 'Lembaga Penduli Yatim Indonesia',
+                'description' => 'Bantu 100 anak yatim berprestasi melanjutkan pendidikan ke jenjang SMA dan Universitas. Dengan beasiswa ini, mimpi mereka untuk meraih masa depan cerah dapat terwujud meski tanpa sosok orangtua.',
+                'img' => 'https://images.unsplash.com/photo-1509062522246-3755977927d7?w=640&h=480&fit=crop',
+                'date' => '2025-07-01',
+                'target' => 60000000,
+            ],
         ];
+        
+        static $index = 0;
+        $campaign = $campaigns[$index % count($campaigns)];
+        $index++;
+        
+        return $campaign;
     }
 }
